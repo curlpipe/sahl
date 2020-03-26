fed4a57776cf10c34fcf224233f929d41292c333
Curly <luke@dominoid.net>
2020-03-26 14:17:03 +0000

    Added basic multiline support

--- sahl.rb
+++ sahl.rb
@@ -5,9 +5,14 @@ Multiline tags
 =end
 
 class Parser
+  attr_reader :multiline
   def initialize(sahl)
     # Load up the raw data and standardize
     @sahl = sahl
+    @multiline = $mltable[@sahl]
+    standardize
+  end
+  def standardize
     @tag = tag
     # Remove whitespace
     if (@sahl.include? @tag+" {")
@@ -18,6 +23,7 @@ class Parser
       @sahl.sub!(@tag+" ", @tag+"{")
       @sahl << "}"
     end
+    return @sahl
   end
   def tag
     # Extract the tag type
@@ -28,33 +34,59 @@ class Parser
     @sahl.match(/{(.*)}/).to_s[1...-1]
   end
   def export
-    @html = "<#{tag}>" + contents + "</#{tag}>"
+    # Export to html
+    @multiline ? @html = "<#{tag}>\n    " + contents + "\n</#{tag}>" : @html = "<#{tag}>" + contents + "</#{tag}>"
   end
 end
 
 def read(file)
+  $mltable = {}
   f = File.new(file, "r").readlines
-  f.map! do |line|
-    line.delete("\n")
+  # Strip blank lines
+  f.map!(&:strip).reject!{ |s| s.empty? }
+  # Strip newlines
+  f.map! { |l| l.delete("\n") }
+  # Splice broken up tags
+  result = []
+  sub = []
+  toggle = false
+  f.each do |x|
+    if (x.strip[-1] == "}" || x.strip == "}") && (!x.include?("{"))
+      sub << x
+      $mltable[x] = toggle
+      toggle = false
+      result << sub.join
+      $mltable[sub.join] = true
+      sub = []
+    elsif !x.include? "{"
+      toggle ? sub << x : result << x
+      $mltable[x] = toggle
+    elsif !x.include? "}"
+      sub << x
+      toggle = true
+      $mltable[x] = toggle
+    else
+      toggle ? sub << x : result << x
+      $mltable[x] = toggle
+    end
   end
-  return f
+  return result
 end
 
 def write
   # Write resultant html to a file
-  #htmlName = ARGV[0][0...ARGV[0].length-4] + "html"
-  #File.open(htmlName, "w") { |f| f.write("this feature aint workin yet boss") }
+  htmlName = ARGV[0].split(".")[1..-1] + ".html"
+  File.open(htmlName, "w") { |f| f.write("this feature aint workin yet boss") }
 end
 
 def convert(input)
   # Convert a line of the file into html
   input.each do |line|
-    # Make a copy of original
-    ori = line[0..-1]
     parser = Parser.new(line)
-    puts "#{ori} -> #{parser.export}"
+    puts parser.export
   end
 end
 
 # Take file argument
-puts convert(read(ARGV[0]))
+convert(read(ARGV[0]))
+#p read(ARGV[0])
